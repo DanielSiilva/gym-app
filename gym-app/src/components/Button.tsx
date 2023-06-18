10b1e1c590497af1262ced1ec4c51d70cf20c061
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-17 22:32:50 -0300

    Feat: Utilizando Variantes

--- gym-app/src/components/Button.tsx
+++ gym-app/src/components/Button.tsx
@@ -1,28 +1,29 @@
-import {Button as ButtonNativeBase, IButtonProps, Text} from 'native-base'
-
+import { Button as ButtonNativeBase, IButtonProps, Text } from 'native-base';
 type Props = IButtonProps & {
-  title: string
+  title: string;
 }
 
-export function Button({title, ...rest}: Props){
-  return(
+export function Button({ title, variant, ...rest }: Props) {
+  return (
     <ButtonNativeBase
       w="full"
       h={14}
-      bg="green.700"
+      bg={variant === 'outline' ? 'transparent' : 'green.700'}
+      borderWidth={variant === 'outline' ? 1 : 0}
+      borderColor="green.500"
       rounded="sm"
       _pressed={{
-        bg: 'green.500'
+        bg: variant === 'outline' ? 'gray.500' : 'green.500'  
       }}
       {...rest}
     >
-      <Text
-        color="white" 
+      <Text 
+        color={variant === 'outline' ? 'green.500' : 'white'}
         fontFamily="heading"
         fontSize="sm"
       >
         {title}
       </Text>
     </ButtonNativeBase>
-  )
+  );
 }
--- gym-app/src/screens/SignIn.tsx
+++ gym-app/src/screens/SignIn.tsx
@@ -39,8 +39,10 @@ export function SignIn() {
           secureTextEntry
         />
 
-        <Button title="Acessar"/>
+        <Button title="Acessar" />
       </Center>
+      
+      <Button title="Criar Conta" variant="outline" />
     </VStack>
   );
 }
