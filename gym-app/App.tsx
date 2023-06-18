cf0f72b3b62c4d67203b80aca7b9dbc348e7dc55
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-17 21:28:49 -0300

    Feat: Customizando o tema default

--- gym-app/App.tsx
+++ gym-app/App.tsx
@@ -1,7 +1,8 @@
-import { StatusBar, Text, View } from 'react-native';
-import {NativeBaseProvider} from 'native-base'
+import { StatusBar, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { THEME } from './src/theme';
 
 import { Loading } from '@components/Loading';
 
@@ -9,7 +10,7 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
