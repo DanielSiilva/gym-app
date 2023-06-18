e1baeccf00c62158f460cf1ff6c701a90ff224d2
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-17 22:10:49 -0300

    Feat: Componente de Input

--- gym-app/src/screens/SignIn.tsx
+++ gym-app/src/screens/SignIn.tsx
@@ -1,7 +1,8 @@
 import { VStack, Image, Text, Center, Heading } from "native-base";
-
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
+
+import { Input } from "@components/Input";
 
 export function SignIn() {
   return (
@@ -12,6 +13,7 @@ export function SignIn() {
         resizeMode="contain"
         position="absolute"
       />
+      
       <Center my={24}>
         <LogoSvg />
         <Text color="gray.100" fontSize="sm">
@@ -23,6 +25,9 @@ export function SignIn() {
         <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
           Acesse a conta
         </Heading>
+
+        <Input placeholder="E-mail" />
+        <Input placeholder="Senha" />
       </Center>
     </VStack>
   );
