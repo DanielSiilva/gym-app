554cb2eccbbba68db74e2540bc8699e38af2fbbe
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-18 21:42:52 -0300

    Feat: Rotas Públicas - Removendo o Header

--- gym-app/src/routes/auth.routes.tsx
+++ gym-app/src/routes/auth.routes.tsx
@@ -15,7 +15,7 @@ export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>
 export function AuthRoutes(){
 
   return(
-    <Navigator>
+    <Navigator screenOptions={{headerShown: false}}>
       <Screen
         name='signIn'
         component={SignIn}
