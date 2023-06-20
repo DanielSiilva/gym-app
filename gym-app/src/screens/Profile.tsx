d26d20ac9c50cde4568f44e018898b14d4b3b86f
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-20 20:49:46 -0300

    Feat: Finalizando as Interfaces - Foto do usuário

--- gym-app/src/screens/Profile.tsx
+++ gym-app/src/screens/Profile.tsx
@@ -1,11 +1,22 @@
-import { VStack, Text } from 'native-base';
+import { VStack, ScrollView, Center } from 'native-base';
 
 import {ScreenHeader} from '@components/ScreenHeader'
+import {UserPhoto} from '@components/UserPhoto'
 
 export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title='Perfil' />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto
+            source={{uri: "https://avatars.githubusercontent.com/u/94769388?v=4"}}
+            alt='Foto do Usuario'
+            size={33}
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   );
 }
