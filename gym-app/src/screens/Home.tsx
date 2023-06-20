5919cfcbb35eb854e88989553412d77a6e287761
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-19 21:27:31 -0300

    Feat: Finalizando as Interfaces - Exibindo o grupo selecionado

--- gym-app/src/screens/Home.tsx
+++ gym-app/src/screens/Home.tsx
@@ -1,16 +1,29 @@
+import {useState} from 'react'
+
 import { HStack, VStack } from 'native-base';
 
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costa')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
       <HStack>
-       <Group name='Costas'/>
-       <Group name='Pernas'/>
+       <Group 
+        name='Costas'
+        isActive={groupSelected === 'costa'}
+        onPress={()=> setGroupSelected('costa')}
+       />
+
+       <Group 
+        name='pernas'
+        isActive={groupSelected === 'pernas'}
+        onPress={()=> setGroupSelected('pernas')}
+       />
       </HStack>
     </VStack>
   );
