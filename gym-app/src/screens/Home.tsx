536e1e385f50690a4d02d97a5e3a0bde3e1c4570
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-19 21:36:50 -0300

    Feat: Finalizando as Interfaces - Listando os grupos

--- gym-app/src/screens/Home.tsx
+++ gym-app/src/screens/Home.tsx
@@ -1,30 +1,36 @@
 import {useState} from 'react'
 
-import { HStack, VStack } from 'native-base';
+import { HStack, VStack, FlatList } from 'native-base';
 
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
 
 export function Home() {
   const [groupSelected, setGroupSelected] = useState('costa')
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro'])
 
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <HStack>
-       <Group 
-        name='Costas'
-        isActive={groupSelected === 'costa'}
-        onPress={()=> setGroupSelected('costa')}
-       />
-
-       <Group 
-        name='pernas'
-        isActive={groupSelected === 'pernas'}
-        onPress={()=> setGroupSelected('pernas')}
-       />
-      </HStack>
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <Group 
+            name={item}
+            isActive={groupSelected === item}
+            onPress={()=> setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   );
 }
