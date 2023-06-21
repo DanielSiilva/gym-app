62b6944bbfb233c0551ee1f6f4772153b9be369e
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-20 21:51:53 -0300

    Feat: Finalizando as Interfaces - Navegando para tela de exercício

--- gym-app/src/screens/Home.tsx
+++ gym-app/src/screens/Home.tsx
@@ -1,15 +1,23 @@
 import {useState} from 'react'
+import {useNavigation} from '@react-navigation/native'
 
 import { HStack, VStack, FlatList, Text, Heading } from 'native-base';
 
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
 import { ExerciseCard } from '@components/ExerciseCard';
+import {AppNavigatorRoutesProps} from "@routes/app.routes"
 
 export function Home() {
   const [groupSelected, setGroupSelected] = useState('Costas')
   const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro'])
   const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terras'])
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate('exercise');
+  }
 
   return (
     <VStack flex={1}>
@@ -49,7 +57,7 @@ export function Home() {
           data={exercises}
           keyExtractor={item => item}
           renderItem={({item}) => (
-            <ExerciseCard />
+            <ExerciseCard onPress={handleOpenExerciseDetails} />
           )}
           showsHorizontalScrollIndicator={false}
           _contentContainerStyle={{
