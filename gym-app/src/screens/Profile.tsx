e4e30e098048706f7518c7ed890dc77bc5921f76
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-20 21:18:19 -0300

    Feat: Efeito Skeleton

--- gym-app/src/screens/Profile.tsx
+++ gym-app/src/screens/Profile.tsx
@@ -1,20 +1,35 @@
-import { VStack, ScrollView, Center } from 'native-base';
+import {useState} from 'react'
+import { VStack, ScrollView, Center, Skeleton } from 'native-base';
 
 import {ScreenHeader} from '@components/ScreenHeader'
 import {UserPhoto} from '@components/UserPhoto'
 
+const PHOTO_SIZE = 33;  
+
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
     <VStack flex={1}>
       <ScreenHeader title='Perfil' />
 
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={{uri: "https://avatars.githubusercontent.com/u/94769388?v=4"}}
-            alt='Foto do Usuario'
-            size={33}
-          />
+          {photoIsLoading ?
+            <Skeleton  
+              w={PHOTO_SIZE} 
+              h={PHOTO_SIZE} 
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+            :
+            <UserPhoto
+              source={{uri: "https://avatars.githubusercontent.com/u/94769388?v=4"}}
+              alt='Foto do Usuario'
+              size={PHOTO_SIZE}
+            />
+          }
         </Center>
       </ScrollView>
     </VStack>
