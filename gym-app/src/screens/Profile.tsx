7ed09aad045c76f77aa82f069a18fc7c75e7bf96
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-23 22:27:34 -0300

    Feat: Galeria de Fotos - Utilizando mensagens toast

--- gym-app/src/screens/Profile.tsx
+++ gym-app/src/screens/Profile.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Alert, TouchableOpacity } from 'react-native';
-import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast} from 'native-base';
 import * as ImagePicker from 'expo-image-picker';
 
 import { ScreenHeader } from '@components/ScreenHeader';
@@ -17,6 +17,8 @@ const PHOTO_SIZE = 33;
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
   const [userPhoto, setUserPhoto] = useState('https://avatars.githubusercontent.com/u/94769388?v=4');
+
+  const toast = useToast()
 
   async function handleUserPhotoSelected(){
     setPhotoIsLoading(true);
@@ -37,7 +39,11 @@ export function Profile() {
         const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri) as FileInfo
 
         if(photoInfo.size && (photoInfo.size  / 1024 / 1024 ) > 5){
-          return Alert.alert('Essa imagem é muito grande. Escolha uma de até 5MB.'); 
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500'
+          })
         }
 
         setUserPhoto(photoSelected.assets[0].uri);
