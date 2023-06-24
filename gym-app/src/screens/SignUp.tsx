c1e4e165c7c9b5c16324f850a7dcdd5c42b5196c
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-24 19:15:12 -0300

    Refactore: Adicionando regras aos input

--- gym-app/src/screens/SignUp.tsx
+++ gym-app/src/screens/SignUp.tsx
@@ -57,6 +57,9 @@ export function SignUp() {
           <Controller 
             control={control}
             name="name"
+            rules={{
+              required: 'Informe o nome.'
+            }}
             render={({ field: { onChange, value } }) => (
               <Input 
                 placeholder="name"
