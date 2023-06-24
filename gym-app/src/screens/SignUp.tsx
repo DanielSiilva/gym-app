693a350c3c74c4666859e07bced261a5582d0269
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-23 22:50:26 -0300

    Feat: Formulários - Inputs com Estados

--- gym-app/src/screens/SignUp.tsx
+++ gym-app/src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import {useState} from "react"
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 
 import LogoSvg from '@assets/logo.svg';
@@ -8,10 +9,24 @@ import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
 
 export function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
   const navigation = useNavigation()
 
   function handleGoBack(){
     navigation.goBack()
+  }
+
+  function handleSignUp() {
+    console.log({
+      name,
+      email,
+      password,
+      passwordConfirm
+    });
   }
 
   return (
@@ -40,20 +55,32 @@ export function SignUp() {
 
           <Input 
             placeholder="Nome"
+            onChangeText={setName}
           />
 
           <Input 
             placeholder="E-mail" 
             keyboardType="email-address"
             autoCapitalize="none"
+            onChangeText={setEmail}
 
           />
           <Input 
             placeholder="Senha" 
             secureTextEntry
+            onChangeText={setPassword}
           />
 
-          <Button title="Criar e acessar" />
+          <Input 
+            placeholder="Confirme a Senha" 
+            secureTextEntry
+            onChangeText={setPasswordConfirm}
+          />
+
+          <Button 
+            title="Criar e acessar"
+            onPress={handleSignUp}
+          />
         </Center>
 
         <Button 
