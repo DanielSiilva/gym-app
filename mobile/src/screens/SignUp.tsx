9aa6387e83280d9056c6d1e9f61a22e8a94ea23b
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-26 21:29:30 -0300

    Feat: HTTP Client - Axios Exceptions

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -1,9 +1,11 @@
+import {Alert} from 'react-native'
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 import {useForm, Controller} from 'react-hook-form'
 import {yupResolver} from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 
 import {api} from '../services/api'
+import axios from 'axios';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -11,6 +13,7 @@ import BackgroundImg from '@assets/background.png';
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
+
 
 type FormDataProps = {
   name: string;
@@ -41,8 +44,14 @@ export function SignUp() {
   }
 
   async function handleSignUp({email, name, password}:FormDataProps) {
-    const response = await api.post('/users',{name, email, password})
-    console.log(response.data);
+    try {
+      const response = await api.post('/users', { name, email, password });
+      console.log(response.data);
+    } catch (error) {
+      if(axios.isAxiosError(error)) {
+        Alert.alert(error.response?.data.message);
+      }
+    }
       
   }
 
