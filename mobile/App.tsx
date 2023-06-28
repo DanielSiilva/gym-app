c960b5150813b438c02bf9a28ee42553367fd75c
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-28 20:13:39 -0300

    Feat: Unificando o create e provider context

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { THEME } from './src/theme';
 
-import {AuthContext} from '@contexts/AuthContext';
+import {AuthContextProvider} from '@contexts/AuthContext';
 
 import { Loading } from '@components/Loading';
 import { Routes } from '@routes/index';
@@ -18,16 +18,9 @@ export default function App() {
         barStyle="light-content"
         translucent
       />
-      <AuthContext.Provider value={{
-        user:{
-          id: '1',
-          name: 'Daniel',
-          email: 'danielsousa@gmail.com',
-          avatar: 'daniel.png'
-        }
-      }}>
+      <AuthContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,8 +1,28 @@
-import { createContext } from "react";
+import { ReactNode, createContext } from "react";
 import { UserDTO } from "@dtos/UserDTO";
 
 export type AuthContextDataProps = {
   user: UserDTO
 }
 
+type AuthContextProviderProps ={
+  children: ReactNode
+}
+
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
+
+export function AuthContextProvider ({children}: AuthContextProviderProps){
+
+  return(
+    <AuthContext.Provider value={{
+      user:{
+        id: '1',
+        name: 'Daniel',
+        email: 'danielsousa@gmail.com',
+        avatar: 'daniel.png'
+      }
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
