38e20c6385d2005aafcfeb39591b1dd8f38425e5
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-28 20:23:41 -0300

    Feat: Compartilhando estados no contexto

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useState } from "react";
 import { UserDTO } from "@dtos/UserDTO";
 
 export type AuthContextDataProps = {
@@ -12,15 +12,16 @@ type AuthContextProviderProps ={
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
 export function AuthContextProvider ({children}: AuthContextProviderProps){
+  const [user, setUser] = useState({
+    id: '1',
+    name: 'Daniel Sous@',
+    email: 'danielsousa@gmail.com',
+    avatar: 'danielGostoso.png'
+  });
 
   return(
     <AuthContext.Provider value={{
-      user:{
-        id: '1',
-        name: 'Daniel Sousa',
-        email: 'danielsousa@gmail.com',
-        avatar: 'danielGostoso.png'
-      }
+      user
     }}>
       {children}
     </AuthContext.Provider>
