4c89a2dccd53956cd9d177ff8e2087149ed31746
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-07-01 20:44:32 -0300

    Feat: Criando conta e logando usuario

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base";
 import {useForm, Controller} from 'react-hook-form'
@@ -5,6 +6,7 @@ import {yupResolver} from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 
 import {api} from '../services/api'
+import { useAuth } from '@hooks/useAuth';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -12,7 +14,6 @@ import BackgroundImg from '@assets/background.png';
 import { AppError } from '@utils/AppError';
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
-
 
 
 
@@ -31,8 +32,10 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
 
   const toast = useToast();
+  const {singIn} = useAuth();
 
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({ 
     resolver: yupResolver(signUpSchema),
@@ -47,9 +50,13 @@ export function SignUp() {
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
     try {
-      const response = await api.post('/users', { name, email, password });
-      console.log(response.data);
+      setIsLoading(true)
+      
+      await api.post('/users', { name, email, password });
+      singIn(email,password)
+
     } catch (error) {
+      setIsLoading(false)
       const isAppError = error instanceof AppError;
 
       const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde';
@@ -149,6 +156,7 @@ export function SignUp() {
           <Button 
             title="Criar e acessar"
             onPress={handleSubmit(handleSignUp)}
+            isLoading={isLoading}
           />
         </Center>
 
