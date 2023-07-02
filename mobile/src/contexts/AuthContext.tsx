91ae6bee100eb4af79b2627d8ae320f0ff330b7b
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-07-02 20:44:30 -0300

    Feat: Removendo o token do storage

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { storageAuthTokenSave, storageAuthTokenGet } from '@storage/storageAuthToken';
+
+import { storageAuthTokenSave, storageAuthTokenGet, storageAuthTokenRemove } from '@storage/storageAuthToken';
 import { storageUserGet, storageUserRemove, storageUserSave } from '@storage/storageUser';
 
 import { api } from '@services/api';
@@ -61,6 +62,8 @@ export function AuthContextProvider({ children }: AuthContextProviderProps)  {
       setIsLoadingUserStorageData(true);
       setUser({} as UserDTO);
       await storageUserRemove();
+      await storageAuthTokenRemove();
+      
     } catch (error) {
       throw error;
     } finally {
--- mobile/src/storage/storageAuthToken.ts
+++ mobile/src/storage/storageAuthToken.ts
@@ -7,6 +7,9 @@ export async function storageAuthTokenSave(token: string) {
 
 export async function storageAuthTokenGet() {
   const token = await AsyncStorage.getItem(AUTH_STORAGE);
-
   return token;
+}
+
+export async function storageAuthTokenRemove() {
+ await AsyncStorage.removeItem(AUTH_STORAGE);
 }
