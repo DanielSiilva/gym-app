81824eddce0c7c2ab7a0e579514d2e840aa832a5
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-07-06 22:43:18 -0300

    Feat: Consumindo o Back-end - Carregando dados do usuário no profile

--- mobile/src/screens/Profile.tsx
+++ mobile/src/screens/Profile.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
-import { Alert, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast} from 'native-base';
+import { Controller, useForm } from 'react-hook-form';
 import * as ImagePicker from 'expo-image-picker';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import * as FileSystem from 'expo-file-system';
+
+import { useAuth } from '@hooks/useAuth';
 
 import { UserPhoto } from '@components/UserPhoto';
 import { Input } from '@components/Input';
@@ -14,11 +17,24 @@ import { FileInfo } from "expo-file-system";
 
 const PHOTO_SIZE = 33;
 
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  oldPassword: string;
+  newPassword: string;
+}
+
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
   const [userPhoto, setUserPhoto] = useState('https://avatars.githubusercontent.com/u/94769388?v=4');
 
   const toast = useToast()
+  const { user } = useAuth();
+  const { control } = useForm<FormDataProps>({ defaultValues: { 
+    name: user.name,
+    email: user.email
+   } });
 
   async function handleUserPhotoSelected(){
     setPhotoIsLoading(true);
@@ -83,14 +99,32 @@ export function Profile() {
               Alterar Foto
             </Text>
           </TouchableOpacity>
-          <Input 
-            bg="gray.600" 
-            placeholder='Nome' 
+          
+          <Controller 
+            control={control}
+            name="name"
+            render={({ field: { value, onChange } }) => (
+              <Input 
+                bg="gray.600" 
+                placeholder='Nome'
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
-          <Input 
-            bg="gray.600" 
-            placeholder="E-mail"
-            isDisabled
+
+          <Controller 
+            control={control}
+            name="email"
+            render={({ field: { value, onChange } }) => (
+              <Input 
+                bg="gray.600" 
+                placeholder="E-mail"
+                isDisabled
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
         
           <Heading color="gray.200" fontSize="md" mb={2} alignSelf="flex-start" mt={12} fontFamily='heading'>
